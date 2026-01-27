1a78272908921ee027ac05eee7cb6e4ef2846279
niespihu <niespihu12@gmail.com>
2026-01-27 18:57:43 -0500

    nuevos cambios

--- app/routes/carro_router.py
+++ app/routes/carro_router.py
@@ -1,6 +1,5 @@
 from typing import List
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
 
 from app.items.carro_model import ProductoActualizado, ProductoCreado, ProductosMensaje
 
@@ -10,12 +9,12 @@ carrito = []
 
 @carro_router.get("/ver")
 def ver_carrito() -> List[ProductoCreado]:
-    return JSONResponse(content=carrito, status_code=200)
+    return carrito
 
 @carro_router.post("/agregar")
 def agregar_carrito(producto: ProductoCreado) -> List[ProductoCreado]:
     carrito.append(producto)
-    return JSONResponse(content=carrito, status_code=201)
+    return carrito
 
 @carro_router.put("/actualizar")
 def actualizar_carrito(producto_actualizado: ProductoActualizado) -> ProductosMensaje:
@@ -23,17 +22,18 @@ def actualizar_carrito(producto_actualizado: ProductoActualizado) -> ProductosMe
         if producto.id == producto_actualizado.id:
             producto.cantidad = producto_actualizado.cantidad
             break
-    return JSONResponse(content={"mensaje": "Producto actualizado correctamente"})
+    return {"mensaje": "Producto actualizado correctamente"}
 
 @carro_router.delete("/eliminar/{id}")
 def eliminar_producto_individual(id: int) -> ProductosMensaje:
+    global carrito
     carrito = [producto for producto in carrito if producto.id != id]
-    return JSONResponse(content={"mensaje": "Producto eliminado correctamente"})
+    return {"mensaje": "Producto eliminado correctamente"}
 
 @carro_router.delete("/eliminar")
 def eliminar_carrito()->ProductosMensaje:
     carrito.clear()
-    return JSONResponse(content={"mensaje": "Producto eliminado correctamente"})
+    return {"mensaje": "Producto eliminado correctamente"}
     
 
 
