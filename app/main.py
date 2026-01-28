43cbb6ff3f306f052aca63ced3f19bb70a6a68c9
niespihu <niespihu12@gmail.com>
2026-01-27 19:40:42 -0500

    cambios origen

--- app/main.py
+++ app/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 
 from app.routes.carro_router import carro_router
 from app.routes.imagen_router import imagen_router
+from app.utils.cors import add_cors_middleware
+from app.utils.http_error_handler import HTTPErrorHandler
 
 
 app = FastAPI()
+
+# Cors Middleware
+add_cors_middleware(app)
+app.add_middleware(HTTPErrorHandler)
 
 app.include_router(imagen_router)
 app.include_router(carro_router)
